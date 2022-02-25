b0d097ff88eab1d2e8015583df804f66072a2b93
Sergio Bravo <sergio.bravo@monstar-lab.com>
2022-02-25 12:20:51 -0500

    Initial commit

---
+++ DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by Sergio Bravo on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
