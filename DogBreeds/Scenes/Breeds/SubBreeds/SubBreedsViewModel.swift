dcfae64b72f5142921c82eb640f8170a4efee29e
Sergio Bravo <sergio.bravo@monstar-lab.com>
2022-02-26 19:50:21 -0500

    BreedImagesView added

--- DogBreeds/Scenes/Breeds/SubBreeds/SubBreedsViewModel.swift
+++ DogBreeds/Scenes/Breeds/SubBreeds/SubBreedsViewModel.swift
@@ -14,6 +14,7 @@ import Domain
     @Published var familyName = ""
     @Published var subBreeds: [DogBreed] = []
     
+    // MARK: - Initializer
     init(dogFamily: DogFamily) {
         self.dogFamily = dogFamily
         
---
+++ DogBreeds/Scenes/Images/Breed/BreedImagesView.swift
@@ -0,0 +1,20 @@
+//
+//  BreedImagesView.swift
+//  DogBreeds
+//
+//  Created by Sergio Bravo on 26/02/22.
+//
+
+import SwiftUI
+
+struct BreedImagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BreedImagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreedImagesView()
+    }
+}
